591c63814e72452a5803664933dce35fb2195043
LittleLebowski <o.f.atas@gmail.com>
2022-12-02 14:25:37 +0300

    Completed Assignment

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import Article from "./Article";
 import Comment from "./Comment";
 
 function App() {
+  class CommentC extends React.Component {
+    render() {
+      return <div>class component Naturally, I agree with this article.</div>;
+    }
+  }
   return (
     <div>
       <Article />
+      <CommentC />
       <Comment />
     </div>
   );
